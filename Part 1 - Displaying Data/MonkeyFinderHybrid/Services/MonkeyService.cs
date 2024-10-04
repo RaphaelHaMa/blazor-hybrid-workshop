42bcf839238644313af69117f823924eae3da806
Raphael <raphael.lukas1983@gmail.com>
2024-10-04 11:43:58 +0200

    Finished Part 1 - Displaying Data

--- Part 1 - Displaying Data/MonkeyFinderHybrid/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinderHybrid/Services/MonkeyService.cs
@@ -1,6 +1,36 @@
+using System.Net.Http.Json;
+
 namespace MonkeyFinderHybrid.Services;
 
 public class MonkeyService
 {
-    
+    private List<Monkey> monkeysList = new();
+
+    private readonly HttpClient _httpClient;
+
+    public MonkeyService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<List<Monkey>> GetMonkeysAsync()
+    {
+        if (monkeysList.Count > 0)
+        {
+            return monkeysList;
+        }
+
+        var response = await _httpClient.GetAsync("https://montemagno.com/monkeys.json");
+        if (response.IsSuccessStatusCode)
+        {
+            var monkeysResult = await response.Content.ReadFromJsonAsync(MonkeyContext.Default.ListMonkey);
+            
+            if (monkeysResult is not null)
+            {
+                monkeysList = monkeysResult;
+            }
+        }
+
+        return monkeysList;
+    }
 }
